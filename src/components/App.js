3c6d34599d0b26399021d7f28d778b0929364587
Sunny  Prakash <sunny.pprakash@gmail.com>
2021-05-09 18:43:43 +0530

    build app

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,27 @@
-import React from "react";
-import '../styles/App.css';
-import Welcome from './Welcome';
+import React, { useState } from "react";
+import "../styles/App.css";
+import Welcome from "./Welcome";
 
 const App = () => {
-  return (
-    <Welcome name="" />
-  )
-}
-
+    const [name, setName] = useState("");
+    const [showMsg, setShowMsg] = useState(false);
+    const onChangeInput = (e) => {
+        setName(e.target.value);
+    };
+    const onClickBtn = () => {
+        setShowMsg(true);
+    };
+    return (
+        <React.Fragment>
+            <form>
+                <input type="text" onChange={(e) => onChangeInput(e)} placeholder="Enter name" />
+                <button type="button" onClick={() => onClickBtn()}>
+                    click me
+                </button>
+            </form>
+            {showMsg ? <Welcome name={name} /> : ""}
+        </React.Fragment>
+    );
+};
 
 export default App;
